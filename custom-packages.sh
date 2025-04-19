3920ad0408a140212194d272c3b32c5c49d30384
404 <aceboy50@gmail.com>
2025-04-20 02:51:19 +0700

    luci-app-easymesh (1)

--- custom-packages.sh
+++ custom-packages.sh
@@ -2,3 +2,8 @@
 
 #git clone --depth 1 --branch master https://github.com/vernesong/OpenClash.git
 #mv OpenClash/luci-app-openclash $BUILDROOT/package
+
+# torguardvpn's easymesh
+
+git clone https://github.com/torguardvpn/luci-app-easymesh.git
+mv luci-app-easymesh $BUILDROOT/package
