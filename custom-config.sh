1f92274210a9ccbc02c6e08c19549f39ac404248
404 <aceboy50@gmail.com>
2025-04-21 02:51:40 +0700

    Remove deprecated

--- custom-config.sh
+++ custom-config.sh
@@ -47,12 +47,9 @@ echo '# CONFIG_PACKAGE_wpad-basic-mbedtls is not set' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_wpad-mbedtls=y' >> $BUILDROOT/.config
 
 #2025 optimization (since that there are many stronger device for using as gateway)
-#echo 'CONFIG_IPV6=n' >> $BUILDROOT/.config
-#echo 'CONFIG_PACKAGE_ip6tables=n' >> $BUILDROOT/.config
 #echo 'CONFIG_PACKAGE_odhcp6c=n' >> $BUILDROOT/.config
-#echo 'CONFIG_PACKAGE_kmod-ipv6=n' >> $BUILDROOT/.config
-#echo 'CONFIG_PACKAGE_kmod-ip6tables=n' >> $BUILDROOT/.config
 #echo 'CONFIG_PACKAGE_odhcpd-ipv6only=n' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-proto-ipv6=n' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-proto-ppp=n' >> $BUILDROOT/.config
-#echo 'CONFIG_PACKAGE_ppp-mod-pppox=n' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_ppp=n' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_ppp-mod-pppoe=n' >> $BUILDROOT/.config
