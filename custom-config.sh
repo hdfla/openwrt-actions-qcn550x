6ac4c740d36042e2c17c9a63b8de791d48008ad7
404 <aceboy50@gmail.com>
2025-04-21 03:08:45 +0700

    Fix custom-config.sh

--- custom-config.sh
+++ custom-config.sh
@@ -23,8 +23,6 @@ echo 'CONFIG_TARGET_ath79=y' >> $BUILDROOT/.config
 echo 'CONFIG_TARGET_ath79_generic=y' >> $BUILDROOT/.config
 echo 'CONFIG_TARGET_ath79_generic_DEVICE_asus_rt-ac59u=y' >> $BUILDROOT/.config
 
-
-
 echo '# CONFIG_PACKAGE_kmod-ath10k-ct is not set' >> $BUILDROOT/.config
 echo '# CONFIG_PACKAGE_ath10k-firmware-qca9888-ct is not set' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_kmod-ath10k=y' >> $BUILDROOT/.config
@@ -46,9 +44,9 @@ echo '# CONFIG_PACKAGE_wpad-basic-mbedtls is not set' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_wpad-mbedtls=y' >> $BUILDROOT/.config
 
 #2025 optimization (since that there are many stronger device for using as gateway)
-echo 'CONFIG_PACKAGE_luci-proto-ipv6 is not set' >> $BUILDROOT/.config
-echo 'CONFIG_PACKAGE_luci-proto-ppp is not set' >> $BUILDROOT/.config
-echo 'CONFIG_PACKAGE_odhcp6c is not set' >> $BUILDROOT/.config
-echo 'CONFIG_PACKAGE_odhcpd-ipv6only is not set' >> $BUILDROOT/.config
-echo 'CONFIG_PACKAGE_ppp is not set' >> $BUILDROOT/.config
-echo 'CONFIG_PACKAGE_ppp-mod-pppoe is not set' >> $BUILDROOT/.config
+echo '# CONFIG_PACKAGE_luci-proto-ipv6 is not set' >> $BUILDROOT/.config
+echo '# CONFIG_PACKAGE_luci-proto-ppp is not set' >> $BUILDROOT/.config
+echo '# CONFIG_PACKAGE_odhcp6c is not set' >> $BUILDROOT/.config
+echo '# CONFIG_PACKAGE_odhcpd-ipv6only is not set' >> $BUILDROOT/.config
+echo '# CONFIG_PACKAGE_ppp is not set' >> $BUILDROOT/.config
+echo '# CONFIG_PACKAGE_ppp-mod-pppoe is not set' >> $BUILDROOT/.config
